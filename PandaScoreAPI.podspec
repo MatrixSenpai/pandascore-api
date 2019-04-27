6effca7abefe5447960f2fedd56e6e4ef84bfae1
Mason Phillips <math.matrix@icloud.com>
2019-04-27 02:15:32 -0700

    Quick fix for get teams endpoint

--- PandaScoreAPI.podspec
+++ PandaScoreAPI.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = "PandaScoreAPI"
-  s.version = "1.0.1"
+  s.version = "1.0.2"
   s.summary = "API lib for PandaScore used in Syqen"
 
   s.homepage = "https://github.com/matrixsenpai/pandascore-api"
--- Sources/panda-api/Endpoints/Teams/GetTeamsEndpoint.swift
+++ Sources/panda-api/Endpoints/Teams/GetTeamsEndpoint.swift
@@ -17,7 +17,7 @@ struct GetTeamsEndpoint: APIRequest {
         case .all: return base + "/teams"
         default:
             guard let id = id else { fatalError("An id must be specified for this type of request!") }
-            return base + "/\(type.rawValue)s/players/\(id)/stats"
+            return base + "/\(type.rawValue)s/\(id)/teams"
         }
     }
     
@@ -30,6 +30,29 @@ struct GetTeamsEndpoint: APIRequest {
         self.type = type
         self.id = id
         self.options = nil
+    }
+}
+
+public struct GetTeamsParameterObject: ParameterObject {
+    public let acronym: [String]?
+    public let id: [Int]?
+    public let name: [String]?
+    public let slug: [String]?
+    
+    public init(acronym: [String]? = nil, id: [Int]? = nil, name: [String]? = nil, slug: [String]?) {
+        self.acronym = acronym
+        self.id = id
+        self.name = name
+        self.slug = slug
+    }
+}
+
+public struct GetTeamsSortObject: SortObject {
+    typealias KeyType = GetTeamsSortKey
+    var keys: [(GetTeamsSortObject.KeyType, Bool)]?
+    
+    public enum GetTeamsSortKey: String {
+        case acronym, id, name, slug
     }
 }
 
